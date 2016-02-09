44240e2bfe1c7e3dd1efd703b219c449fc8078b1
Chi Kong <kchi2000@yahoo.com>
2016-02-09 01:36:34 -0800

    Test - added functions to controller

--- TicTacToe_v2/TicTacToe_v2/controller.cpp
+++ TicTacToe_v2/TicTacToe_v2/controller.cpp
@@ -17,4 +17,24 @@ string getInput()
 	return strInput;
 }
 
-//update grid
+//Validates player's move and updates grid
+bool updateGrid(string(*grid)[3][3], int x, int y, string playerSign)
+{
+	if ((*grid)[x][y].length() == 0)
+	{
+		(*grid)[x][y] = playerSign;
+		return true;
+	}
+	else if ((*grid)[x][y].length() > 0)
+	{
+		cout << "spot is already filled\n";
+		return false;
+	}
+	else
+	{
+		//cout << x << " " << y << endl;
+		return false;
+	}
+}
+
+//win logic
